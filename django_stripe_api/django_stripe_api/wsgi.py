6522a5884a68aa554584e829b30d47cf9a830127
Kapusta-fairy <dknenorm63@gmail.com>
2023-02-09 11:57:08 +0300

    rename folders to lowercase

--- django_stripe_api/django_stripe_api/wsgi.py
+++ django_stripe_api/django_stripe_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DjangoStripeAPI project.
+WSGI config for django_stripe_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoStripeAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_stripe_api.settings')
 
 application = get_wsgi_application()
