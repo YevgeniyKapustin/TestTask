209f712fbb5db386fca7ed594f26b2796461211b
Kapusta-fairy <dknenorm63@gmail.com>
2023-02-10 12:58:37 +0300

    create session.py

--- django_stripe_api/api/views.py
+++ django_stripe_api/api/views.py
@@ -1,27 +1,14 @@
-import stripe
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 from item.models import Item
+from item.session import get_stripe_session
 
 
 class ItemAPIView(viewsets.ViewSet):
     @staticmethod
     def session_id_for_item(request, pk=None):
         item = get_object_or_404(Item, pk=pk)
-        session = stripe.checkout.Session.create(
-            line_items=[{
-                'price_data': {
-                    'currency': 'usd',
-                    'product_data': {
-                        'name': item.name,
-                    },
-                    'unit_amount': item.price,
-                },
-                'quantity': 1,
-            }],
-            mode='payment',
-            success_url='http://127.0.0.1:8000/',
-        )
+        session = get_stripe_session(item)
         return Response(session.id)
--- django_stripe_api/item/session.py
+++ django_stripe_api/item/session.py
@@ -1,19 +1,19 @@
-import stripe as stripe
+import stripe
 
 
-def get_session_id_for_item() -> int:
+def get_stripe_session(item):
     session = stripe.checkout.Session.create(
         line_items=[{
             'price_data': {
                 'currency': 'usd',
                 'product_data': {
-                    'name': 'T-shirt',
+                    'name': item.name,
                 },
-                'unit_amount': 2000,
+                'unit_amount': item.price,
             },
             'quantity': 1,
         }],
         mode='payment',
         success_url='http://127.0.0.1:8000/',
-    )
-    return session.id
+        )
+    return session
