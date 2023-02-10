15b6fbab647cc8407595567a4af0957e3dc9510e
Kapusta-fairy <dknenorm63@gmail.com>
2023-02-10 13:10:45 +0300

    fix link

--- django_stripe_api/api/urls.py
+++ django_stripe_api/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from api.views import ItemAPIView
 
 urlpatterns = [
-    path('buy/<int:pk>/', ItemAPIView.as_view({'get': 'session_id_for_item'})),
+    path('buy/<int:pk>/', ItemAPIView.as_view({'get': 'session_id_for_item'}),
+         name='buy'),
 ]
