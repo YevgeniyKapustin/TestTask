a1146a8e3b48015b6d2a40d473390de7cdfa9274
Kapusta-fairy <dknenorm63@gmail.com>
2023-02-11 08:53:25 +0300

    add __str__ into Item

--- django_stripe_api/item/models.py
+++ django_stripe_api/item/models.py
@@ -5,3 +5,6 @@ class Item(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=150)
     price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
