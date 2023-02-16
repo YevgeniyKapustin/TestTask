23a115f304e769bf48b351e0cb2b8c3ac154fff8
Kapusta-fairy <dknenorm63@gmail.com>
2023-02-16 14:51:05 +0300

    add docker file

--- django_stripe_api/django_stripe_api/settings.py
+++ django_stripe_api/django_stripe_api/settings.py
@@ -57,10 +57,11 @@ WSGI_APPLICATION = 'django_stripe_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432),
     }
 }
 
